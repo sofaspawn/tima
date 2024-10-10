af305a7a8351c0b68e561da0ac91a6c386e3400e
sofaspawn <h.m1nus.work@gmail.com>
2024-10-10 21:38:54 +0530

    time formatting going strong

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ fn formattime(anchor: DateTime<Local>)->String{
     let eminutes = elapsed.num_minutes();
     let eseconds = elapsed.num_seconds();
 
-    let ohours = if ehours<10{format!("0{}", ehours)} else {ehours.to_string()};
-    let ominutes = if eminutes<10{format!("0{}", eminutes)} else {eminutes.to_string()};
-    let oseconds = if eseconds<10{format!("0{}", eseconds)} else {eseconds.to_string()};
+    let hdiv = (ehours/60) as i64;
+    let mdiv = (eminutes/60) as i64;
+    let sdiv = (eseconds/60) as i64;
+
+    let ohours = if ehours<10{format!("0{}", ehours)} else if hdiv>0{(ehours - hdiv*60).to_string()} else {ehours.to_string()};
+    let ominutes = if eminutes<10{format!("0{}", eminutes)} else if mdiv>0{(eminutes - mdiv*60).to_string()} else {eminutes.to_string()};
+    let oseconds = if eseconds<10{format!("0{}", eseconds)} else if sdiv>0{(eseconds - sdiv*60).to_string()} else {eseconds.to_string()};
 
     format!("{}:{}:{}", ohours, ominutes, oseconds)
 }
@@ -27,7 +31,9 @@ fn main() {
         .title("tima")
         .build();
 
-    let font = 200;
+    let base_font = 200;
+    let mut scale = 1;
+    let font = base_font * scale;
     let init = Local::now();
 
     rl.set_target_fps(60);
@@ -38,6 +44,14 @@ fn main() {
         unsafe{
             if IsKeyDown(KeyboardKey::KEY_Q as i32){
                 break;
+            }
+            if IsKeyDown(KeyboardKey::KEY_EQUAL as i32){
+                scale+=1;
+            }
+            if IsKeyDown(KeyboardKey::KEY_MINUS as i32){
+                if scale>1{
+                    scale-=1;
+                }
             }
         }
         let twid = rl.measure_text(thms.as_str(), font);
