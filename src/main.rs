d6c774e560ada6bc6219c802fad9c22fc92075c8
sofaspawn <h.m1nus.work@gmail.com>
2024-10-18 12:16:43 +0530

    errorrsss

--- src/main.rs
+++ src/main.rs
@@ -43,84 +43,7 @@ fn handle_args(args: &Vec<String>)->Option<String>{
 }
 
 fn stopwatch(font_path: &str){
-    let (mut rl, thread) = raylib::init()
-        .size(WIDTH, HEIGHT)
-        .title("tima")
-        .build();
-
-    let font = rl.load_font(&thread, font_path).unwrap();
-
-    let mut scale = 400.0;
-    let font_size = 1;
-    let init = Local::now();
-
-    let mut cwid = rl.get_screen_width();
-    let mut chit = rl.get_screen_height();
-
-    let mut camera = ffi::Camera2D{
-        offset: ffi::Vector2{x:(cwid/2) as f32, y:(chit/2) as f32}, 
-        target: ffi::Vector2{x:(cwid/2) as f32, y:(chit/2) as f32},
-        rotation: 0.0,
-        zoom: 1.0
-    };
-
-    rl.set_target_fps(60);
-
-    let mut vib_counter = 0;
-
-    while !rl.window_should_close() {
-        let thms = elapsedtime(init);
-
-        unsafe{
-            if IsKeyDown(KeyboardKey::KEY_Q as i32){
-                break;
-            }
-            if IsKeyDown(KeyboardKey::KEY_EQUAL as i32){
-                scale+=2.0;
-                //println!("{scale}");
-            }
-            if IsKeyDown(KeyboardKey::KEY_MINUS as i32){
-                if scale>5.0{
-                    scale-=2.0;
-                }
-                //println!("{scale}");
-            }
-            if IsKeyDown(KeyboardKey::KEY_ZERO as i32){
-                scale = 400.0;
-            }
-        }
-
-        let twid = rl.measure_text(thms.as_str(), (font_size as f32 * scale) as i32);
-        let thi = (font_size as f32 * scale) as i32;
-
-        cwid = rl.get_screen_width();
-        chit = rl.get_screen_height();
-
-        let mut pos = ffi::Vector2{
-            x:((cwid - twid)/2) as f32,
-            y:((chit-thi)/2) as f32,
-        };
-
-        timemvmnt(vib_counter, &mut pos);
-
-        camera.zoom = scale as f32;
-        //camera.target = ffi::Vector2{x:(cwid/2) as f32, y:(chit/2) as f32};
-        //camera.offset = ffi::Vector2{x:(cwid/2) as f32, y:(chit/2) as f32};
-        camera.offset = pos;
-        camera.target = pos;
-
-        vib_counter+=1;
-
-        unsafe{
-            ffi::BeginMode2D(camera);
-        }
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::BLACK);
-        d.draw_text_ex(&font, thms.as_str(), pos, font_size as f32 * scale, 10.0, Color::WHITE);
-        unsafe{
-            ffi::EndMode2D();
-        }
-    }
+    let thms = elapsedtime(init);
 }
 
 fn clock(){
@@ -161,7 +84,6 @@ fn main() {
         ,
         None => ()
     }
-
 
     let (mut rl, thread) = raylib::init()
         .size(WIDTH, HEIGHT)
